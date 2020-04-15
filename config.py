00971d7d042eb77b211434a45907c0dd4317674a
walter-clayton <walter.h.clayton@gmail.com>
2020-04-15 14:50:57 +0100

    basic set up

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = 'H4S6%YI}r,w5#"jya3#<\x0c>gh'
 
-FB_APP_ID = 3084275108271122
+FB_APP_ID = 898223130622563
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
--- fbapp/views.py
+++ fbapp/views.py
@@ -5,22 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    description = """Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif,
-     tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et
-      que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème.
-       N'aurais-tu pas un petit problème d'autorité ? ;-)"""
+    description = """Welcome"""
     return render_template('index.html',
-                            user_name="Lorenzo",
+                            user_name="Walter",
                             user_image=url_for('static', filename="tmp/cover_111823112767411.jpg"),
-                            description=description)
+                            description=description,
+                            blur = True)
 
 
 @app.route('/result/')
 def result():
-    description = """Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée !
-     Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !"""
+    description = """Results Page !"""
     return render_template('result.html',
-                            user_name="Lorenzo",
+                            user_name="Walter",
                             user_image=url_for('static', filename="tmp/cover_111823112767411.jpg"),
                             description=description)
 
